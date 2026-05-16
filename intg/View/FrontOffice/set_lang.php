8588c889f32841fe061700107eb5a0a8dfbc7c1a
roua kadri <kadri.roua@esprit.tn>
2026-05-16 09:12:33 -0930

    amelioration

--- intg/View/FrontOffice/set_lang.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (session_status() === PHP_SESSION_NONE) session_start();
-$lang = $_GET['lang'] ?? 'fr';
-if (in_array($lang, ['fr', 'en'])) {
-    $_SESSION['lang'] = $lang;
-}
-$ref = $_SERVER['HTTP_REFERER'] ?? 'accueil.php';
-header('Location: ' . $ref);
-exit;
